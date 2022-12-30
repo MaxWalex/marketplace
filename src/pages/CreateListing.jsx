0fe2add22438f1e5db37bd2133845940fdf3abe5
Maxim Chervonoshtan <chervonoshtan02@gmail.com>
2022-12-30 19:57:55 +0200

    update

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -100,10 +100,10 @@ function CreateListing() {
       geolocation.lat = data.results[0]?.geometry.location.lat ?? 0
       geolocation.lng = data.results[0]?.geometry.location.lng ?? 0
 
-      location =
-        data.status === 'ZERO_RESULTS'
-          ? undefined
-          : data.results[0]?.formatted_address
+      location = data.status === 'ZERO_RESULTS'
+                  ? undefined
+                  : data.results[0]?.formatted_address
+        
 
       // if (location === undefined || location.includes('undefined')) {
       //   setLoading(false)
